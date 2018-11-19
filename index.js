bcc830ee89d1c7c149e05d117d6f69a19b3f2cc5
magic-onion <alexander.m.truong@gmail.com>
2018-11-18 21:52:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  return drivers.filter(function (name) {
+    return string.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch (drivers, letter) {
+  return drivers.filter(function (name) {
+    return letter[0].toLowerCase() === name[0].toLowerCase();
+  })
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(function (driver) {
+    return string.toLowerCase() === driver.name.toLowerCase();
+  })
+}
